1401a9db7968c83ce11ba60d4d13b9f58938ccbe
mayuxiang1995 <207658567@qq.com>
2019-07-25 15:38:39 +0800

    自定义网络（全注释）

---
+++ MnistCNN/module.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+# 创建卷积神经网络
+class Cnn(nn.Module):
+    def __init__(self, in_dim, n_class):  # in_dim是传入图片的通道数，n_class是最终输出数据的种类，在数字识别中有10个数字
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, 32, 3),  # 第一层通道为1，卷积层为32，卷积核大小为3*3
+            nn.ReLU(True),  # 激活函数用ReLU
+            nn.MaxPool2d(2, 2),  # 池化层用2*2最大池化
+            nn.Conv2d(32, 64, 3),  # 第二层卷积用32通道，64层，大小为3*3
+            nn.ReLU(True),
+            nn.MaxPool2d(2, 2),
+            nn.Dropout2d(),
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(12 * 12 * 64, 120),
+            nn.Dropout2d(),
+            nn.Linear(128, n_class)
+        )
+
+    def forward(self, x):  # x为输入的图片
+        out = self.conv(x)
+        out = out.view(out.size(0), 12 * 12 * 64)  # 卷积池化完成以后需要将其展开，第一个参数为-1，就是默认维度的意思，out.size是你输入图片的维度。
+        out = self.fc(out)
+        # return F.log_softmax(out, dim=1)# 将得出的值返回成一个
+        return out
