83f8019268093cbf7c36cacb337994e9832ba326
mayuxiang1995 <207658567@qq.com>
2019-07-25 20:20:13 +0800

    ①第一次调试，尝试增加训练次数，并首先增大lr，然后减小lr，再增大的策略。准确率由91-92提升到98-99

--- MnistCNN/train.py
+++ MnistCNN/train.py
@@ -8,7 +8,7 @@ from func import train, test
 model = Cnn(1, 10)
 train_loader, test_loader = getData()
 epochs = 12  # 训练次数
-lr = 0.001  # 学习率
+lr = 0.01  # 学习率
 momentum = 0.6  # 动量
 model_file = 'model_file'
 log_interval = 20
