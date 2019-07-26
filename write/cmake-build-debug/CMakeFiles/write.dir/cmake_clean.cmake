9b5a5978adba4bcbbf98208d8cfd6efde70cd7e3
zf <1690065310@qq.com>
2019-07-26 21:28:19 +0800

    write代码第一次提交

---
+++ write/cmake-build-debug/CMakeFiles/write.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/write.dir/main.cpp.o"
+  "write.pdb"
+  "write"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/write.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
