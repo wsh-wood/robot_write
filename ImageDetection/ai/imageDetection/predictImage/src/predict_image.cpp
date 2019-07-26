bf1210bb5ca9c1d6bbe5178c93a1f47400c01c5b
mayuxiang1995 <207658567@qq.com>
2019-07-26 18:19:51 +0800

    配置CMakeLists，注意阅读readme中对此工程的描述

---
+++ ImageDetection/ai/imageDetection/predictImage/src/predict_image.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <torch/torch.h>
+#include <torch/script.h>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cout << "predictImage" << endl;
+    return 0;
+}
